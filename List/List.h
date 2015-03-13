8479741bd433475c52146667bd1e3971bfbac9c7
Neil Pankey <npankey@gmail.com>
2015-03-13 13:56:59 -0700

    Initial commit

---
+++ List/List.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for List.
+extern double ListVersionNumber;
+
+/// Project version string for List.
+extern const unsigned char ListVersionString[];
+
