5a087ae3f92dd03898761c54c8e9efdc048ad64a
Neil Pankey <npankey@gmail.com>
2015-03-19 23:30:49 -0700

    Internalize ListNode

--- List/ListNode.swift
+++ List/ListNode.swift
@@ -1,11 +1,11 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 
 /// Generic, singly-linked list of nodes. The basis for `List` and other higher level collections.
-public final class ListNode<T> {
+internal final class ListNode<T> {
     // MARK: Constructors
 
     /// Initialize a new node with `value` and no tail.
-    public convenience init(_ value: T) {
+    internal convenience init(_ value: T) {
         self.init(value, nil)
     }
 
@@ -13,7 +13,7 @@ public final class ListNode<T> {
     ///
     /// Ideally this would be a failable initializier but limitations in Swift prevent that
     /// prevent that on class types - http://stackoverflow.com/a/26497229/1999152.
-    public static func create<S: SequenceType where S.Generator.Element == T>(values: S) -> ListEnds<T> {
+    internal static func create<S: SequenceType where S.Generator.Element == T>(values: S) -> ListEnds<T> {
         var ends = ListEnds<T>()
         Swift.map(values) { ends.append($0) }
         return ends
@@ -22,25 +22,25 @@ public final class ListNode<T> {
     // MARK: Properties
 
     /// The `value` at the this node.
-    public var value: T
+    internal var value: T
 
     /// The remainder of the list.
-    public var next: ListNode?
+    internal var next: ListNode?
 
     /// The `last` node in the list. This is O(n) since it walks the entire list.
-    public var last: ListNode {
+    internal var last: ListNode {
         return next?.last ?? self
     }
 
     /// MARK: Operations
 
     /// Prefixes the receiver with a new value returning the created `ListNode`.
-    public func insertBefore(value: T) -> ListNode {
+    internal func insertBefore(value: T) -> ListNode {
         return ListNode(value, self)
     }
 
     /// Replaces `next` of the receiver with a new value returning the created `ListNode` which points at the replaced `next`.
-    public func insertAfter(value: T) -> ListNode {
+    internal func insertAfter(value: T) -> ListNode {
         next = ListNode(value, next)
         return next!
     }
@@ -65,10 +65,10 @@ public final class ListNode<T> {
 // MARK: SequenceType
 
 extension ListNode : SequenceType {
-    public typealias Generator = GeneratorOf<ListNode>
+    internal typealias Generator = GeneratorOf<ListNode>
 
     /// Create a `Generator` that enumerates all the nodes.
-    public func generate() -> Generator {
+    internal func generate() -> Generator {
         var node: ListNode? = self
 
         return Generator {
@@ -79,7 +79,7 @@ extension ListNode : SequenceType {
     }
 
     /// Create a `Generator` that enumerates all the values of nodes.
-    public func values() -> GeneratorOf<T> {
+    internal func values() -> GeneratorOf<T> {
         var node: ListNode? = self
 
         return GeneratorOf<T> {
@@ -96,7 +96,7 @@ extension ListNode : SequenceType {
 
 extension ListNode {
     /// Maps values in self with `transform` to create a new list.
-    public func map<U>(transform: T -> U) -> ListEnds<U> {
+    internal func map<U>(transform: T -> U) -> ListEnds<U> {
         return reduce(ListEnds<U>()) { (var ends, node) in
             ends.append(transform(node.value))
             return ends
@@ -104,7 +104,7 @@ extension ListNode {
     }
 
     /// Filters values in self with `predicate` to create a new list.
-    public func filter(predicate: T -> Bool) -> ListEnds<T> {
+    internal func filter(predicate: T -> Bool) -> ListEnds<T> {
         return self.reduce(ListEnds<T>()) { (var ends, node) in
             if predicate(node.value) {
                 ends.append(node.value)
@@ -114,12 +114,12 @@ extension ListNode {
     }
 
     /// Takes values up to `tail` to create a new list.
-    public func takeUntil(tail: ListNode?) -> ListEnds<T> {
+    internal func takeUntil(tail: ListNode?) -> ListEnds<T> {
         return takeWhile(ListEnds<T>()) { $0 != tail }
     }
 
     /// Takes values while `predicate` returns true to create a new list.
-    public func takeWhile(predicate: ListNode -> Bool) -> ListEnds<T> {
+    internal func takeWhile(predicate: ListNode -> Bool) -> ListEnds<T> {
         return takeWhile(ListEnds<T>(), predicate)
     }
 
@@ -134,7 +134,7 @@ extension ListNode {
     }
 
     /// Reduces all nodes to with `transform`.
-    public func reduce<U>(initial: U, _ transform: (U, ListNode) -> U) -> U {
+    internal func reduce<U>(initial: U, _ transform: (U, ListNode) -> U) -> U {
         // TODO Would be nice if this were lazy
         let reduction = transform(initial, self)
         return next?.reduce(reduction, transform) ?? reduction
@@ -147,19 +147,19 @@ extension ListNode : Equatable {
 }
 
 /// Determines if two nodes are equal via identity.
-public func == <T> (lhs: ListNode<T>, rhs: ListNode<T>) -> Bool {
+internal func == <T> (lhs: ListNode<T>, rhs: ListNode<T>) -> Bool {
     return lhs === rhs
 }
 
 // MARK: ListEnds
 
-/// Wrapper for the `head` and `tail` of list nodes.
-public struct ListEnds<T> {
-    var head: ListNode<T>?
-    var tail: ListNode<T>?
+/// Wrapper for the `head` and `tail` of list nodes. Generally used as an accumulator when mapping, filter, etc.
+internal struct ListEnds<T> {
+    internal var head: ListNode<T>?
+    internal var tail: ListNode<T>?
 
     /// Appends a new `value` to the current `tail` and updates it.
-    mutating func append(value: T) {
+    internal mutating func append(value: T) {
         if let node = tail?.insertAfter(value) {
             tail = node
         } else {
@@ -168,4 +168,3 @@ public struct ListEnds<T> {
         }
     }
 }
-
